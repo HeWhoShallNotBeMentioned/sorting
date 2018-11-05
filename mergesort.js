4310cb69590bd088ba497faa05c96d964d8c4835
Chris Underwood <cunderwoodmn@gmail.com>
2018-11-05 15:34:50 -0600

    placed recursive code to work

--- mergesort.js
+++ mergesort.js
@@ -11,7 +11,9 @@ function split(wholeArray) {
   return [firstHalf, secondHalf];
 }
 
-function merge([arr1, arr2]) {
+function merge(arr1, arr2) {
+  arr1 = arr1.slice();
+  arr2 = arr2.slice();
   let mergedArr = [];
 
   while (arr1.length > 0 && arr2.length > 0) {
@@ -31,4 +33,19 @@ function merge([arr1, arr2]) {
   mergedArr = mergedArr.concat(arr2);
 
   return mergedArr;
+}
+
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+  //return mergeSort(merge(split(array)));
+
+  let [firstHalf, secondHalf] = split(array);
+
+  let firstHalfSorted = mergeSort(firstHalf);
+  console.log('firstHalfSorted ', firstHalfSorted, firstHalf);
+  let secondHalfSorted = mergeSort(secondHalf);
+  console.log('secondHalfSorted ', secondHalfSorted, secondHalf);
+  return merge(firstHalfSorted, secondHalfSorted);
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -7,21 +7,19 @@ describe('Split Array function', function() {
 
 describe('Merge Array function', function() {
   it('is able to merge 2 sorted arrays into one function', function() {
-    expect(merge([[2], [1, 3, 4, 5]])).toEqual([1, 2, 3, 4, 5]);
+    expect(merge([2], [1, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
     //expect(window.swap.calls.count()).toEqual(2);
   });
 
   it('is able to merge 2 sorted arrays into one function', function() {
-    expect(merge([[2, 6, 7, 8], [1, 3, 4, 5]])).toEqual([
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-    ]);
+    expect(merge([2, 6, 7, 8], [1, 3, 4, 5])).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    //expect(window.swap.calls.count()).toEqual(2);
+  });
+});
+
+describe('Merge Array function', function() {
+  it('is able to merge 2 sorted arrays into one function', function() {
+    expect(mergeSort([2, 5, 3, 1, 4])).toEqual([1, 2, 3, 4, 5]);
     //expect(window.swap.calls.count()).toEqual(2);
   });
 });
